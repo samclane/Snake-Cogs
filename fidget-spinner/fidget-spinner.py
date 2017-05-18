4da40863ad559510fcd61941bba1eff3fba529ec
Sawyer McLane <samclane@gmail.com>
2017-05-18 01:49:26 -0500

    Removed something not ready for prime time

--- fidget-spinner/fidget-spinner.py
+++ fidget-spinner/fidget-spinner.py
@@ -3,20 +3,20 @@ from discord.ext import commands
 from .utils import checks
 import time
 
-SPINNER_A = "`  |  \n" \
-          " / \\`"
+SPINNER_A = "  |  \n" \
+            " / \\"
 
-
-SPINNER_B = "`  /\n" \
+SPINNER_B = "  /\n" \
             "- \n" \
-            "  \\`"
+            "  \\"
 
-SPINNER_C = "` \\ /\n" \
-            "  |`"
+SPINNER_C = " \\ /\n" \
+            "  |"
 
-SPINNER_D = "`  \\\n" \
+SPINNER_D = "  \\\n" \
             "    -\n" \
-            "  /`"
+            "  /"
+
 
 class FidgetSpinner:
     def __init__(self, bot):
@@ -25,13 +25,16 @@ class FidgetSpinner:
     @commands.group(pass_context=False, no_pm=True)
     async def spin(self):
         """Change DamnDog Settings"""
-        msg = await self.bot.say(SPINNER_A)
+        embed = discord.Embed()
+        embed.add_field(name="Spinner", value=SPINNER_A)
+        msg = await self.bot.say(embed=embed)
         for state in [SPINNER_B, SPINNER_C, SPINNER_D, SPINNER_A]:
             time.sleep(.5)
-            msg = await self.bot.edit_message(msg, state)
+            embed = discord.Embed()
+            embed.add_field(name="Spinner", value=state)
+            msg = await self.bot.edit_message(msg, embed=embed)
+
 
 def setup(bot):
     n = FidgetSpinner(bot)
     bot.add_cog(n)
-
-
