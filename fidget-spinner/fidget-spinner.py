3e48490c707690c93225126570188a0e2a387ff1
Sawyer McLane <samclane@gmail.com>
2017-05-18 02:36:15 -0500

    Removed something not ready for prime time

--- fidget-spinner/fidget-spinner.py
+++ fidget-spinner/fidget-spinner.py
@@ -12,8 +12,8 @@ class FidgetSpinner:
     @commands.group(pass_context=False, no_pm=True)
     async def spin(self):
         im = Image.open("data\\fidget-spinner\\spinner.png")
-        msg = self.pixelize(im)
-        await self.bot.say(msg)
+        txt = self.pixelize(im)
+        msg = await self.bot.say(txt)
         for deg in range(0, 360, 90):
             im = im.rotate(deg)
             txt = self.pixelize(im)
