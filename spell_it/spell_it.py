ea47bb8010a7a5fe5a6be386ca09c780c9eb14bf
Sawyer McLane <samclane@gmail.com>
2017-07-02 18:00:38 -0500

    Added spell_it module

---
+++ spell_it/spell_it.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+
+
+class SpellIt:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        content = message.content
+        if message.author.bot:
+            return
+        user = message.author.name
+        if 'bb' in content:
+            meme_ref = 'bb'
+        elif 'pp' in content:
+            meme_ref = 'pp'
+        await self.bot.send_message(message.channel, "{} you said {}".format(user, meme_ref))
+
+
+def setup(bot):
+    n = SpellIt(bot)
+    bot.add_listener(n.on_message, "on_message")
+    bot.add_cog(n)
