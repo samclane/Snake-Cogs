22e73ade5a8d9e1186392f6dcdf6308810dd4521
Sawyer McLane <samclane@gmail.com>
2017-07-02 18:21:24 -0500

    Added spell_it module

--- spell_it/spell_it.py
+++ spell_it/spell_it.py
@@ -7,21 +7,21 @@ class SpellIt:
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(self, message):
+    async def message_recv(self, message):
         content = message.content
         if message.author.bot:
             return
-        user = message.author.name
+        user = message.author
         if 'bb' in content:
             meme_ref = 'bb'
         elif 'pp' in content:
             meme_ref = 'pp'
         else:
             return
-        await self.bot.send_message(message.channel, "{} you said {}".format(user, meme_ref))
+        await self.bot.send_message(message.channel, "{} you said {}".format(user.mention, meme_ref))
 
 
 def setup(bot):
     n = SpellIt(bot)
-    bot.add_listener(n.on_message, "on_message")
+    bot.add_listener(n.message_recv, "on_message")
     bot.add_cog(n)
