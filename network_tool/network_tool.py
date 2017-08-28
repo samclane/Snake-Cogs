5a76191306b2c1631b907adda7c8b468a49431fc
Sawyer McLane <samclane@gmail.com>
2017-08-27 19:58:15 -0500

    Moving to different weboscket frmwrk

--- network_tool/network_tool.py
+++ network_tool/network_tool.py
@@ -5,10 +5,8 @@ import websockets
 class NetworkTool:
     def __init__(self, bot):
         self.bot = bot
-        start_server = websockets.serve(hello, 'localhost', 8765)
+        self.start_server = websockets.serve(hello, 'localhost', 8765)
 
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
 
     async def hello(websocket, path):
         name = await websocket.recv()
@@ -22,5 +20,9 @@ class NetworkTool:
 
 def setup(bot):
     n = NetworkTool(bot)
+
+    asyncio.get_event_loop().run_until_complete(n.start_server)
+    asyncio.get_event_loop().run_forever()
+    
     bot.add_cog(n)
 
