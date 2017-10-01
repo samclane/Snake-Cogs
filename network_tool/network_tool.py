bd373453c1e69f1bd9f9317886bedc31d64501fd
Sawyer McLane <samclane@gmail.com>
2017-10-01 14:23:45 -0500

    Updated websocket

--- network_tool/network_tool.py
+++ network_tool/network_tool.py
@@ -13,15 +13,15 @@ class NetworkTool:
             await websocket.send("hello")
             print("> {}".format("hello"))
         elif msg == "counter":
-            await websocket.send("{}".format(self.bot.counter))
-            print("> {}".format("{}".format(self.bot.counter)))
+            await websocket.send("{}".format(str(self.bot.counter)))
+            print("> {}".format("{}".format(str(self.bot.counter))))
 
 
 
 def setup(bot):
     n = NetworkTool(bot)
 
-    asyncio.get_event_loop().run_until_complete(websockets.serve(n.hello, 'localhost', 8773))
+    asyncio.get_event_loop().run_until_complete(websockets.serve(n.hello, 'localhost', 8774))
 
     bot.add_cog(n)
 
