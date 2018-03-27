c60ec2dce81028bcd5dc2d5dddeeefa55f596a95
Jeremy Wood <farachan@gmail.com>
2018-03-27 18:55:45 -0400

    Fixed error when users don't have weapons.

--- armorsmith/armorsmith.py
+++ armorsmith/armorsmith.py
@@ -601,13 +601,17 @@ class Armorsmith:
         await self.bot.say("{}, do you accept this challenge?".format(user.mention))
         msg = await self.bot.wait_for_message(timeout=15, author=user, content='yes')
         if msg and msg.content == "yes":
-            battle_text, author_won = self.duel(author, user, settings)
-            for page in pagify(battle_text, shorten_by=12):
-                await self.bot.say(box(page, lang="py"))
-            if author_won:
-                self.bank.transfer_credits(user, author, wager)
+            result = await self.duel(author, user, settings)
+            if result != None:
+                battle_text, author_won = result
+                for page in pagify(battle_text, shorten_by=12):
+                    await self.bot.say(box(page, lang="py"))
+                if author_won:
+                    self.bank.transfer_credits(user, author, wager)
+                else:
+                    self.bank.transfer_credits(author, user, wager)
             else:
-                self.bank.transfer_credits(author, user, wager)
+                await self.bot.say("Challenge cancelled!")
         else:
             await self.bot.say("Challenge declined.")
 
